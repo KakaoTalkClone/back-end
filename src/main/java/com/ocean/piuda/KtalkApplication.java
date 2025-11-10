88e3eb003bafbbd6a58e4c1182179c7737bd4659
jungjiyu <142137932+jungjiyu@users.noreply.github.com>
2025-11-10 17:22:56 +0900

    init

--- src/main/java/com/ocean/piuda/KtalkApplication.java
+++ src/main/java/com/ocean/piuda/KtalkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PiudaApplication {
+public class KtalkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PiudaApplication.class, args);
+		SpringApplication.run(KtalkApplication.class, args);
 	}
 
 }
